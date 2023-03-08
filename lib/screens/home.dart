472ea31e3bc2ab6daabc69cd1ee297b884e669b9
Omar Maamon <omarmaamoun6@gmail.com>
2023-03-08 20:09:59 +0200

    create bloc provider and consumer to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/cubit/news_cubit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('News App'),
+    return BlocProvider(
+      create: (context) => NewsCubit(),
+      child: BlocConsumer<NewsCubit, NewsState>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        builder: (context, state) {
+          var cubit = NewsCubit.get(context);
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('News App'),
+            ),
+            body: cubit.screens[cubit.currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: cubit.currentIndex,
+              onTap: (index) {
+                cubit.changeScreen(index);
+              },
+              items:cubit.bottomNav,
+            ),
+          );
+        },
       ),
-      body: 
-      BottomNavigationBar(items: [],),
     );
   }
 }
